313accdce9919867fe687628ebf64a2c6ebe440c
Aleksei Chkmrv <chkmrv@gmail.com>
2020-01-21 16:38:36 +0100

    Add-test data

--- react-admin-app/src/App.js
+++ react-admin-app/src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//src/App.js
+import React, { Component } from "react";
+import { Admin, Resource } from "react-admin";
+import  { UserList, UserEdit }  from './users';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import jsonServerProvider from "ra-data-json-server";
+
+const dataProvider =
+    jsonServerProvider("https://jsonplaceholder.typicode.com");
+class App extends Component {
+    render() {
+        return (
+            <Admin dataProvider={dataProvider}>
+                <Resource name="users" list={UserList} />
+            </Admin>
+        );
+    }
 }
-
 export default App;
